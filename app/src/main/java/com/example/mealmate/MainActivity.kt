15c6dd0b5b9abb734eecc059a9a70b037cbd98cc
Ben Djibril <konedjibril2004@gmail.com>
2025-05-03 17:53:34 +0100

    Configure project for Git and remove default UI content
    - Configured the project to use Git for version control.
    - Removed the default UI content, including the `Greeting` composable and its preview.

--- app/src/main/java/com/example/mealmate/MainActivity.kt
+++ app/src/main/java/com/example/mealmate/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MealMateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // A surface container using the 'background' color from the theme
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MealMateTheme {
-        Greeting("Android")
     }
 }
