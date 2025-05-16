b144ef9ea493d6c6934ba1299c0a598f0a0fc9c8
Ben Djibril <konedjibril2004@gmail.com>
2025-05-16 06:33:45 +0100

    Update DashboardScreen layout based on screen size
    - Implemented a responsive layout for `DashboardScreen` using `BoxWithConstraints`.
    - Added conditional rendering to display a navigation rail for wider screens (> 600dp) and a bottom app bar for smaller screens.
    - Included `MyTopBar` and `MyBottomAppBar` components in the smaller screen layout.
    - Included `MyNavigationOnRail` component in the wider screen layout.
    - Added padding based on `Scaffold` padding values.

--- app/src/main/java/com/example/mealmate/ui/view/dashboardScreen/DashboardScreen.kt
+++ app/src/main/java/com/example/mealmate/ui/view/dashboardScreen/DashboardScreen.kt
@@ -1,13 +1,64 @@
 package com.example.mealmate.ui.view.dashboardScreen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.mealmate.data.source.model.User
+import com.example.mealmate.ui.view.components.myBottomAppBar.MyBottomAppBar
+import com.example.mealmate.ui.view.components.myBottomAppBar.MyNavigationOnRail
+import com.example.mealmate.ui.view.components.myTopAppBar.MyTopBar
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun DashboardScreen(
     navController: NavController,
     currentUser: User? = null
 ) {
-    //
+    val user = currentUser?: return
+
+
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()){
+        if(maxWidth > 600.dp){
+            Row(modifier = Modifier.fillMaxSize()){
+                MyNavigationOnRail(navController = navController)
+
+                // Contenu principal de l'écran
+                BoxWithConstraints(modifier = Modifier.fillMaxSize())
+                {
+                    //
+                }
+            }
+        }else{
+            Scaffold(
+                topBar = {
+                    MyTopBar(
+                        user = user,
+                        modifier = Modifier,
+                        onNotificationClick = { /*--Rien pour le moment--*/ },
+                        onSearchClick = { /*--Rien pour le moment--*/  }
+                    )
+                },
+                bottomBar = {
+                    MyBottomAppBar(navController = navController)
+                }
+            ) { paddingValues ->
+                BoxWithConstraints(modifier = Modifier.padding(paddingValues)) {
+                    val columnCount = when {
+                        maxWidth > 1200.dp -> 4// pc/web
+                        maxWidth > 600.dp -> 2//tablette
+                        else -> 1//smartphone
+                    }
+
+
+                }
+            }
+        }
+    }
 }
